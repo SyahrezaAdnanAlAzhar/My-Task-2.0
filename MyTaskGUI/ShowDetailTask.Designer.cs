9f305ad905dc3aaed44124b0e1d45e195782858f
Syahreza Adnan Al Azhar <notsupersyahreza@student.telkomuniversity.ac.id>
2024-06-26 10:06:03 +0700

    menambahkan gui show detail task

---
+++ MyTaskGUI/ShowDetailTask.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace MyTaskGUI
+{
+    partial class ShowDetailTask
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label8 = new System.Windows.Forms.Label();
+            this.labelJudul = new System.Windows.Forms.Label();
+            this.panelSideBar = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelDeskripsi = new System.Windows.Forms.Label();
+            this.labelTanggalMulai = new System.Windows.Forms.Label();
+            this.labelTanggalSelesai = new System.Windows.Forms.Label();
+            this.labelJenisTugas = new System.Windows.Forms.Label();
+            this.labelPrioritas = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(68)))), ((int)(((byte)(102)))));
+            this.label8.Location = new System.Drawing.Point(422, 574);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 25);
+            this.label8.TabIndex = 34;
+            this.label8.Text = "Status";
+            // 
+            // labelJudul
+            // 
+            this.labelJudul.AutoSize = true;
+            this.labelJudul.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelJudul.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(68)))), ((int)(((byte)(102)))));
+            this.labelJudul.Location = new System.Drawing.Point(524, 114);
+            this.labelJudul.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelJudul.Name = "labelJudul";
+            this.labelJudul.Size = new System.Drawing.Size(65, 25);
+            this.labelJudul.TabIndex = 32;
+            this.labelJudul.Text = "Judul";
+            // 
+            // panelSideBar
+            // 
+            this.panelSideBar.AutoScroll = true;
+            this.panelSideBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(68)))), ((int)(((byte)(102)))));
+            this.panelSideBar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSideBar.Location = new System.Drawing.Point(0, 0);
+            this.panelSideBar.Margin = new System.Windows.Forms.Padding(4);
+            this.panelSideBar.Name = "panelSideBar";
+            this.panelSideBar.Size = new System.Drawing.Size(255, 703);
+            this.panelSideBar.TabIndex = 31;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(68)))), ((int)(((byte)(102)))));
+            this.label7.Location = new System.Drawing.Point(405, 528);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 25);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "Prioritas";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(68)))), ((int)(((byte)(102)))));
+            this.label6.Location = new System.Drawing.Point(380, 475);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(131, 25);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "Jenis Tugas";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(68)))), ((int)(((byte)(102)))));
+            this.label4.Location = new System.Drawing.Point(351, 364);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(149, 25);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Tanggal Mulai";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(68)))), ((int)(((byte)(102)))));
+            this.label3.Location = new System.Drawing.Point(405, 170);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 25);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Deskripsi";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(68)))), ((int)(((byte)(102)))));
+            this.label1.Location = new System.Drawing.Point(447, 114);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 25);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Judul";
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(68)))), ((int)(((byte)(102)))));
+            this.buttonBack.FlatAppearance.BorderSize = 0;
+            this.buttonBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBack.ForeColor = System.Drawing.Color.White;
+            this.buttonBack.Location = new System.Drawing.Point(529, 621);
+            this.buttonBack.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(161, 47);
+            this.buttonBack.TabIndex = 18;
+            this.buttonBack.Text = "BACK";
+            this.buttonBack.UseVisualStyleBackColor = false;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(68)))), ((int)(((byte)(102)))));
+            this.label2.Location = new System.Drawing.Point(324, 48);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 31);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "UPDATE TASK";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(197)))), ((int)(((byte)(201)))), ((int)(((byte)(216)))));
+            this.panel1.Controls.Add(this.labelStatus);
+            this.panel1.Controls.Add(this.labelPrioritas);
+            this.panel1.Controls.Add(this.labelJenisTugas);
+            this.panel1.Controls.Add(this.labelTanggalSelesai);
+            this.panel1.Controls.Add(this.labelTanggalMulai);
+            this.panel1.Controls.Add(this.labelDeskripsi);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.labelJudul);
+            this.panel1.Controls.Add(this.panelSideBar);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.buttonBack);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1236, 703);
+            this.panel1.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(68)))), ((int)(((byte)(102)))));
+            this.label5.Location = new System.Drawing.Point(335, 418);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(169, 25);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "Tanggal Selesai";
+            // 
+            // labelDeskripsi
+            // 
+            this.labelDeskripsi.AutoSize = true;
+            this.labelDeskripsi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDeskripsi.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(68)))), ((int)(((byte)(102)))));
+            this.labelDeskripsi.Location = new System.Drawing.Point(524, 170);
+            this.labelDeskripsi.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelDeskripsi.Name = "labelDeskripsi";
+            this.labelDeskripsi.Size = new System.Drawing.Size(92, 25);
+            this.labelDeskripsi.TabIndex = 35;
+            this.labelDeskripsi.Text = "Deskripsi";
+            // 
+            // labelTanggalMulai
+            // 
+            this.labelTanggalMulai.AutoSize = true;
+            this.labelTanggalMulai.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTanggalMulai.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(68)))), ((int)(((byte)(102)))));
+            this.labelTanggalMulai.Location = new System.Drawing.Point(529, 364);
+            this.labelTanggalMulai.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelTanggalMulai.Name = "labelTanggalMulai";
+            this.labelTanggalMulai.Size = new System.Drawing.Size(135, 25);
+            this.labelTanggalMulai.TabIndex = 36;
+            this.labelTanggalMulai.Text = "Tanggal mulai";
+            // 
+            // labelTanggalSelesai
+            // 
+            this.labelTanggalSelesai.AutoSize = true;
+            this.labelTanggalSelesai.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTanggalSelesai.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(68)))), ((int)(((byte)(102)))));
+            this.labelTanggalSelesai.Location = new System.Drawing.Point(529, 418);
+            this.labelTanggalSelesai.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelTanggalSelesai.Name = "labelTanggalSelesai";
+            this.labelTanggalSelesai.Size = new System.Drawing.Size(154, 25);
+            this.labelTanggalSelesai.TabIndex = 37;
+            this.labelTanggalSelesai.Text = "Tanggal Selesai";
+            // 
+            // labelJenisTugas
+            // 
+            this.labelJenisTugas.AutoSize = true;
+            this.labelJenisTugas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelJenisTugas.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(68)))), ((int)(((byte)(102)))));
+            this.labelJenisTugas.Location = new System.Drawing.Point(529, 475);
+            this.labelJenisTugas.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelJenisTugas.Name = "labelJenisTugas";
+            this.labelJenisTugas.Size = new System.Drawing.Size(120, 25);
+            this.labelJenisTugas.TabIndex = 38;
+            this.labelJenisTugas.Text = "Jenis Tugas";
+            // 
+            // labelPrioritas
+            // 
+            this.labelPrioritas.AutoSize = true;
+            this.labelPrioritas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPrioritas.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(68)))), ((int)(((byte)(102)))));
+            this.labelPrioritas.Location = new System.Drawing.Point(529, 528);
+            this.labelPrioritas.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelPrioritas.Name = "labelPrioritas";
+            this.labelPrioritas.Size = new System.Drawing.Size(82, 25);
+            this.labelPrioritas.TabIndex = 39;
+            this.labelPrioritas.Text = "Prioritas";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(68)))), ((int)(((byte)(102)))));
+            this.labelStatus.Location = new System.Drawing.Point(529, 574);
+            this.labelStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(68, 25);
+            this.labelStatus.TabIndex = 40;
+            this.labelStatus.Text = "Status";
+            // 
+            // ShowDetailTask
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1236, 703);
+            this.Controls.Add(this.panel1);
+            this.Name = "ShowDetailTask";
+            this.Text = "ShowDetailTask";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label labelJudul;
+        private System.Windows.Forms.Panel panelSideBar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Label labelPrioritas;
+        private System.Windows.Forms.Label labelJenisTugas;
+        private System.Windows.Forms.Label labelTanggalSelesai;
+        private System.Windows.Forms.Label labelTanggalMulai;
+        private System.Windows.Forms.Label labelDeskripsi;
+    }
+}
